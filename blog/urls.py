a61107b917af390910d811f1c2434cbd2e1e1663
Lemon <962650082@qq.com>
2017-01-24 19:30:14 +0800

    Add edit page

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^welcome/', views.welcome),
-    url(r'^index/', views.index),
+    url(r'^index/$', views.index),
     url(r'^article/(?P<article_id>[0-9]+)$', views.article_page, name='article_page'),
+    url(r'^edit/$', views.edit_page, name='edit_page'),
+    url(r'^edit/action$', views.edit_action, name='edit_action')
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,9 +9,21 @@ def welcome(request):
 
 def index(request):
     articles = models.Article.objects.all()
-    return render(request,'blog/index1.html', {'articles': articles})
+    return render(request, 'blog/index1.html', {'articles': articles})
 
 
 def article_page(request, article_id):
     article = models.Article.objects.get(pk=article_id)
-    return render(request,'blog/article_page.html', {'article': article})
+    return render(request, 'blog/article_page.html', {'article': article})
+
+
+def edit_page(request):
+    return render(request, 'blog/edit_page.html')
+
+
+def edit_action(request):
+    title = request.POST.get('title', 'TITLE')
+    content = request.POST.get('content', 'CONTENT')
+    models.Article.objects.create(title=title, content=content)
+    articles = models.Article.objects.all()
+    return render(request, 'blog/index1.html', {'articles': articles})
